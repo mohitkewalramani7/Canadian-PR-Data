fb35876fd80988b61a9dc07874811b4b74e676b7
Mohit Kewalramani <mohitkewalramani74@gmail.com>
2020-04-27 21:44:35 -0700

    First draft of Section A complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import SectionA from './SectionA'
+import SectionB from './SectionB'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 style={{marginBottom: '100px'}}>
+          A PR Application to Canada (Comprehensive Ranking System)
+        </h2>
       </header>
+      <div id="currentScore">
+        <SectionA/>
+        <SectionB/>
+      </div>
+      <div id="entryChance">
+        <h5>Scores and chances</h5>
+      </div>
     </div>
   );
 }
